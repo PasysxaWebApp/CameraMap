f805fb26bc28a10d251be02f3b3a71803a1da145
jzhu063-macpro <zhujing0866@126.com>
2017-04-15 18:55:33 +0800

    change systemtitle

--- WebUI/Configs/SystemConfig.cs
+++ WebUI/Configs/SystemConfig.cs
@@ -3,6 +3,6 @@
 // ReSharper disable once ClassNeverInstantiated.Global
     public class SystemConfig
     {
-        public const string SystemTitle = "广电网络系统";
+        public const string SystemTitle = "陕西高速公路信息管理系统";
     }
 }
